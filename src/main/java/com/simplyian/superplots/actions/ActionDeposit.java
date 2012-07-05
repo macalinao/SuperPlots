44d442a8f42aa99948f50c9b8ea415dd93aac3f3
Ian Macalinao <ianmacalinao@gmail.com>
2012-07-05 17:21:07 -0500

    Deposit action

---
+++ src/main/java/com/simplyian/superplots/actions/ActionDeposit.java
@@ -0,0 +1,58 @@
+package com.simplyian.superplots.actions;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import com.simplyian.superplots.MsgColor;
+import com.simplyian.superplots.SuperPlots;
+import com.simplyian.superplots.plot.Plot;
+
+public class ActionDeposit extends BaseAction {
+
+    public ActionDeposit(SuperPlots main) {
+        super(main);
+    }
+
+    @Override
+    public void perform(Player player, List<String> args) {
+        Plot plot = main.getPlotManager().getPlotAt(player.getLocation());
+        if (plot == null) {
+            player.sendMessage(MsgColor.ERROR + "You are not in a plot.");
+            return;
+        }
+
+        if (args.size() <= 0) {
+            player.sendMessage(MsgColor.ERROR
+                    + "You did not specify an amount of money to deposit.");
+            return;
+        }
+
+        String amountStr = args.get(0);
+        int amount = 0;
+        try {
+            amount = Integer.parseInt(amountStr);
+        } catch (NumberFormatException ex) {
+            player.sendMessage(MsgColor.ERROR_HILIGHT + amountStr
+                    + MsgColor.ERROR
+                    + " is not a valid amount of money to deposit.");
+            return;
+        }
+
+        double balance = main.getEconomy().getBalance(player.getName());
+        if (balance < amount) {
+            player.sendMessage(MsgColor.ERROR
+                    + "You don't have that much money; you only have "
+                    + MsgColor.ERROR_HILIGHT + "D" + balance + MsgColor.ERROR
+                    + ".");
+            return;
+        }
+
+        plot.addFunds(amount);
+        main.getEconomy().subtractBalance(player.getName(), amount);
+        player.sendMessage(MsgColor.SUCCESS_HILIGHT + "D" + amount
+                + MsgColor.SUCCESS + " have been deposited into "
+                + MsgColor.SUCCESS_HILIGHT + plot.getName() + MsgColor.SUCCESS
+                + ".");
+    }
+}
---
+++ src/test/java/com/simplyian/superplots/actions/ActionDepositTest.java
@@ -0,0 +1,140 @@
+package com.simplyian.superplots.actions;
+
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.simplyian.superplots.EconHook;
+import com.simplyian.superplots.SPSettings;
+import com.simplyian.superplots.SuperPlots;
+import com.simplyian.superplots.plot.Plot;
+import com.simplyian.superplots.plot.PlotManager;
+
+public class ActionDepositTest {
+    private SuperPlots main;
+    private ActionDeposit action;
+    private PlotManager plotManager;
+    private Player player;
+    private EconHook econ;
+
+    @Before
+    public void setup() {
+        main = mock(SuperPlots.class);
+        action = new ActionDeposit(main);
+
+        econ = mock(EconHook.class);
+        when(main.getEconomy()).thenReturn(econ);
+
+        plotManager = mock(PlotManager.class);
+        when(main.getPlotManager()).thenReturn(plotManager);
+
+        SPSettings settings = mock(SPSettings.class);
+        when(main.getSettings()).thenReturn(settings);
+        when(settings.getInfluenceMultiplier()).thenReturn(1.5);
+        when(settings.getInitialPlotSize()).thenReturn(10);
+
+        player = mock(Player.class);
+        when(player.getName()).thenReturn("albireox");
+    }
+
+    @After
+    public void tearDown() {
+        main = null;
+        action = null;
+        plotManager = null;
+        player = null;
+    }
+
+    @Test
+    public void test_perform_notInPlot() {
+        World world = mock(World.class);
+        Location playerLoc = new Location(world, 0, 0, 0);
+        when(plotManager.getPlotAt(playerLoc)).thenReturn(null);
+
+        List<String> args = Arrays.asList("asdf");
+        action.perform(player, args);
+
+        verify(player).sendMessage(contains("not in a plot"));
+    }
+
+    @Test
+    public void test_perform_noAmount() {
+        World world = mock(World.class);
+        Location playerLoc = new Location(world, 0, 0, 0);
+        when(player.getLocation()).thenReturn(playerLoc);
+        Plot plot = mock(Plot.class);
+        when(plotManager.getPlotAt(playerLoc)).thenReturn(plot);
+        when(plot.isOwner("albireox")).thenReturn(true);
+        when(player.getName()).thenReturn("albireox");
+
+        List<String> args = Arrays.asList();
+        action.perform(player, args);
+
+        verify(player).sendMessage(contains("did not specify"));
+    }
+
+    @Test
+    public void test_perform_notANumber() {
+        World world = mock(World.class);
+        Location playerLoc = new Location(world, 0, 0, 0);
+        when(player.getLocation()).thenReturn(playerLoc);
+        Plot plot = mock(Plot.class);
+        when(plotManager.getPlotAt(playerLoc)).thenReturn(plot);
+        when(plot.isOwner("albireox")).thenReturn(true);
+        when(player.getName()).thenReturn("albireox");
+        when(econ.getBalance("albireox")).thenReturn(200.0);
+
+        List<String> args = Arrays.asList("400x");
+        action.perform(player, args);
+
+        verify(player).sendMessage(contains("not a valid amount"));
+    }
+
+    @Test
+    public void test_perform_notEnoughMoney() {
+        World world = mock(World.class);
+        Location playerLoc = new Location(world, 0, 0, 0);
+        when(player.getLocation()).thenReturn(playerLoc);
+        Plot plot = mock(Plot.class);
+        when(plotManager.getPlotAt(playerLoc)).thenReturn(plot);
+        when(plot.isOwner("albireox")).thenReturn(true);
+        when(player.getName()).thenReturn("albireox");
+        when(econ.getBalance("albireox")).thenReturn(200.0);
+
+        List<String> args = Arrays.asList("400");
+        action.perform(player, args);
+
+        verify(player).sendMessage(contains("don't have that much money"));
+    }
+
+    @Test
+    public void test_perform_success() {
+        World world = mock(World.class);
+        Location playerLoc = new Location(world, 0, 0, 0);
+        when(player.getLocation()).thenReturn(playerLoc);
+        Plot plot = mock(Plot.class);
+        when(plotManager.getPlotAt(playerLoc)).thenReturn(plot);
+        when(plot.isOwner("albireox")).thenReturn(true);
+        when(player.getName()).thenReturn("albireox");
+        when(econ.getBalance("albireox")).thenReturn(400.0);
+
+        List<String> args = Arrays.asList("400");
+        action.perform(player, args);
+
+        verify(player).sendMessage(contains("have been deposited"));
+        verify(plot).addFunds(400);
+        verify(econ).subtractBalance("albireox", 400);
+    }
+
+}
